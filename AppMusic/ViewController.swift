d2dcb06853116c7ca77133beed9617fdf77e8045
Gabriel Mors <gabrielmors210@gmail.com>
2023-08-21 22:02:18 -0400

    fazendo configuracoes iniciais do projeto

---
+++ AppMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMusic
+//
+//  Created by Gabriel Mors  on 21/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
