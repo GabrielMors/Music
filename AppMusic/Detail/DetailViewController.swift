ea724fb892a08cddcfa1c28a8680e00b9b6814a4
Gabriel Mors <gabrielmors210@gmail.com>
2023-08-23 17:37:27 -0400

    criado file da tela de detail

---
+++ AppMusic/Detail/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  AppMusic
+//
+//  Created by Gabriel Mors  on 23/08/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ AppMusic/Detail/Screen/DetailScreen.swift
@@ -0,0 +1,19 @@
+//
+//  DetailScreen.swift
+//  AppMusic
+//
+//  Created by Gabriel Mors  on 23/08/23.
+//
+
+import UIKit
+
+class DetailScreen: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
