fe5f81ac7d08aea377a4fc9392b29af6e579c3b3
Gabriel Mors <gabrielmors210@gmail.com>
2023-08-23 13:23:16 -0400

    criando views para cardActionVIew

---
+++ AppMusic/CustomView/CardActionView.swift
@@ -0,0 +1,67 @@
+//
+//  CardActionView.swift
+//  AppMusic
+//
+//  Created by Gabriel Mors  on 23/08/23.
+//
+
+import UIKit
+
+class CardActionView: UIView {
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+//      Todos elementos teram o mesmo espaçamento
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    lazy var downloadView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var notInterestedView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var playView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var likeView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var moreView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private func addSubViews() {
+        addSubview(downloadView)
+        addSubview(notInterestedView)
+        addSubview(playView)
+        addSubview(likeView)
+        addSubview(moreView)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
